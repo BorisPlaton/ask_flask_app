ff520729b8221ff7ed170bf172be413adcaa7d13
Kirill Panchencko <videokirills@gmail.com>
2022-03-06 04:22:21 +0200

    register/login

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, session, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
+import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
+
+
+def user_session():
+    """Возвращает данные пользователя, если тот зашёл в свой аккаунт"""
+    user_data = None
+    if "user" in session:
+        db = get_db()
+        cur = db.execute("""
+            SELECT * FROM user
+            WHERE user_name = (?);
+        """, [session["user"]])
+        user_data = cur.fetchone()
+    return user_data
 
 
 def connect_db():  # Подключение базы данных
-    sql = sqlite3.connect("food.db")
+    sql = sqlite3.connect("ask_db.db")
     sql.row_factory = sqlite3.Row
     return sql
 
@@ -24,16 +40,41 @@ def close_db(error):  # Закрытие соединения с базой да
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    user = user_session()
+    return render_template("home.html", user=user)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":  # Если пользователь регистрируется
+        db = get_db()
+        user_name = request.form["login"]
+        password = generate_password_hash(request.form["password"], method="sha256")  # Хэшируем пароль пользователя
+        db.execute("""
+            INSERT INTO user (user_name, password, expert, admin)
+            VALUES
+                (?, ?, ?, ?);
+        """, [user_name, password, False, False])
+        db.commit()
+        session["user"] = user_name
+        return redirect(url_for("home"))
+
     return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":    # Если пользователь входит
+        db = get_db()
+        user_name = request.form["login"]
+        cur = db.execute("""
+            SELECT password FROM user
+            WHERE user_name = (?);
+        """, [user_name])
+        if check_password_hash(cur.fetchone()["password"], request.form["password"]):
+            session["user"] = user_name
+            return redirect(url_for("home"))
+
     return render_template("login.html")
 
 
